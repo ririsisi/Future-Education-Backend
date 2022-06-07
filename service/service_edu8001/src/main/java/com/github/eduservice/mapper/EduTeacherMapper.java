3b4b96fbbec6a9647036bbd69c9aa148c4e89bee
niqi <syq278872032@gmail.com>
2022-06-07 21:21:28 +0800

    init project

---
+++ service/service_edu8001/src/main/java/com/github/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.github.eduservice.mapper;
+
+import com.github.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author Niqi
+ * @since 2022-06-07
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ service/service_edu8001/src/main/java/com/github/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.github.eduservice.service.impl;
+
+import com.github.eduservice.entity.EduTeacher;
+import com.github.eduservice.mapper.EduTeacherMapper;
+import com.github.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author Niqi
+ * @since 2022-06-07
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
